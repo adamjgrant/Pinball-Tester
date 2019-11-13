a00500041abebd57b93156e8e3b8c02bb76fbb8d
Adam Grant <hello@adamgrant.me>
2019-11-13 13:51:38 -0800

    Rather functional

--- js/api/quizzer.js
+++ js/api/quizzer.js
@@ -15,20 +15,29 @@ m$.quizzer.api({
     _$(".active_quiz")[0].classList.add("active");
     _$(".inactive_quiz")[0].classList.remove("active");
 
-    _$.api.focus_input();
-    _$.api.reset_card_timer();
     _$.api.next_card(); 
   }, 
 
-  next_card: (_$, options) => { 
+  next_card: function(_$, options) { 
+    if (options && options.time_expired) {
+      _$.api.submit({ submission: _$(".submission")[0].value });
+    }
+
     this.slides_completed++;
 
     if (m$.settings.quiz_type == "numbers") { 
       [this.card.question, this.card.answer] = this.deck.pop(); 
+      _$(".submission")[0].type = "number";
     } 
     else { 
       [this.card.answer, this.card.question] = this.deck.pop();
+      _$(".submission")[0].type = "text";
     }
+
+    _$(".display")[0].innerHTML = this.card.question;
+    _$(".submission")[0].value = "";
+    _$.api.focus_input();
+    _$.api.reset_card_timer();
   },
 
   stop_quiz: (_$, options) => {
@@ -42,30 +51,32 @@ m$.quizzer.api({
   },
 
   kill_card_timer: function(_$, options) {
-    _$.api.set_clock({ 
-      time: m$.utilities.convert_seconds_to_time({ seconds: 0 }) 
+    _$.api.set_clock_face({ 
+      time: m$.utilities.api.convert_seconds_to_time({ seconds: 0 }) 
     }); 
     clearInterval(this.timer);
   },
 
   start_card_timer: function(_$, options) {
-    _$.api.set_clock({ time: m$.settings.time }); 
-    this.timer = setInterval(_$.api.next_card, m$.settings.speed_in_seconds * 1000);
-    _$.api.start_clock();
+    this.timer = setInterval(() => {
+      _$.api.next_card({ time_expired: true });
+    }, m$.settings.speed_in_seconds * 1000);
+    _$.api.reset_clock_display_timer();
   },
 
   reset_card_timer: (_$, options) => {
     _$.api.kill_card_timer();
     _$.api.start_card_timer();
-  }
+  },
 
   kill_clock_display_timer: function(_$, options) {
     clearInterval(this.clock_display_timer);
   }, 
 
   start_clock_display_timer: function(_$, options) {
-    this.clock_display_timer = setInterval(_$.api.update_clock_face, 1000);
     this.time_remaining_in_seconds = m$.settings.speed_in_seconds;
+    _$.api.update_clock_face();
+    this.clock_display_timer = setInterval(_$.api.update_clock_face, 1000);
   },
 
   reset_clock_display_timer: function(_$, options) {
@@ -75,7 +86,54 @@ m$.quizzer.api({
 
   update_clock_face: function(_$, options) {
     this.time_remaining_in_seconds--; 
-    _$(".clock")[0].innerHTML = m$.utilities.api.convert_seconds_to_time({ seconds: this.time_remaining_in_seconds });
-    if (this.time_remaining_in_seconds <= 0) _$.api.next_card();
-  }
+    _$.api.set_clock_face({ seconds: this.time_remaining_in_seconds });
+    if (this.time_remaining_in_seconds < 0) _$.api.next_card();
+  },
+
+  set_clock_face: (_$, options) => { 
+    _$(".clock")[0].innerHTML = m$.utilities.api.convert_seconds_to_time({ seconds: options.seconds });
+    var percent_complete;
+
+    if (options.seconds > 0) {
+      percent_complete = 100 - ((options.seconds + 1)/m$.settings.speed_in_seconds * 100)
+    } else {
+      percent_complete = 100;
+    }
+
+    _$("progress")[0].value = percent_complete;
+  },
+
+  validate_character: (_$, options) => { 
+    // TODO: This doesn't accept the command key, such as command+a
+    if (!/\d/.test(options.value)) return options.e.preventDefault();
+  },
+
+  submit: function(_$, options) {
+    if (this.card.answer == options.submission) {
+      _$.api.grade_correct();
+    }
+    else {
+      _$.api.grade_incorrect();
+    }
+
+    _$.api.next_card();
+  }, 
+
+  grade_correct: function(_$, options) {
+    this.number_correct++;
+    var nav = $("[data-component='navigation']")[0];
+    nav.classList.add("correct");
+    setTimeout(() => {
+      nav.classList.remove("correct");
+    }, 300);
+  },
+
+  grade_incorrect: function(_$, options) {
+    this.number_incorrect++;
+    var nav = $("[data-component='navigation']")[0];
+    nav.classList.add("incorrect");
+    setTimeout(() => {
+      nav.classList.remove("incorrect");
+    }, 300);
+  },
 });
--- js/events/quizzer.js
+++ js/events/quizzer.js
@@ -1,5 +1,14 @@
 m$.quizzer.events(_$ => {
-  _$("button")[0].addEventListener("click", () => {
-    _$.api.start_quiz()
+  _$("button")[0].addEventListener("click", _$.api.start_quiz); 
+
+  var numerical_input = _$("input[type='number'].submission");
+  if (numerical_input) {
+    numerical_input[0].addEventListener("keypress", e => {
+      return _$.api.validate_character({ value: String.fromCharCode(e.charCode), e: e });
+    });
+  }
+
+  _$(".submission")[0].addEventListener("keypress", e => {
+    if (e.key == "Enter") _$.api.submit({ submission: e.target.value });
   });
 });
