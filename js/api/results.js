88eed0a115546d887c37c6f7c2423f09fb0134dd
Adam Grant <hello@adamgrant.me>
2019-11-13 17:05:41 -0800

    Checkpoint

--- js/api/results.js
+++ js/api/results.js
@@ -9,7 +9,6 @@ m$.results.api({
     };
 
     this.ReportCard.prototype.percent_grade = function() {
-      console.log(this.correct, this.total);
       return this.correct <= 0 ? "0%" : `${this.correct / this.total * 100}%`
     }
 
@@ -40,18 +39,21 @@ m$.results.api({
   },
 
   register_new_score: function(_$, options) {
-    var score,
-        tbody;
+    score = new this.ReportCard(new Date().getTime(), 0, 0, 0)
+  },
 
-    score = new this.ReportCard(
-      new Date().getTime(), 
-      m$.quizzer.number_correct, 
-      m$.quizzer.number_incorrect, 
-      m$.settings.slides_per_session
-    ); 
+  update_last_score: function(_$, options) {
+    var score = this.scores[this.scores.length - 1]; 
+    score.correct   = m$.quizzer.number_correct;
+    score.incorrect = m$.quizzer.number_incorrect; 
+    score.total     = m$.settings.slides_per_session;
+  },
 
-    this.scores.push(score);
+  finalize_last_score: function(_$, options) {
+    var tbody, 
+        score;
     
+    score = this.scores[this.scores.length - 1];
     tbody = _$("table tbody")[0];
     tbody.innerHTML = `
       <tr>
