6e926c46c7f82661b7052d6497e923c91603fc1b
Adam Grant <hello@adamgrant.me>
2019-11-13 21:57:00 -0800

    We're viable

--- js/events/navigation.js
+++ js/events/navigation.js
@@ -1,4 +1,6 @@
 m$.navigation.events(_$ => {
+  _$("button")[0].addEventListener("click", m$.quizzer.api.start_quiz); 
+
   _$("a").forEach((el, index) => {
     ((el, index) => {
       el.addEventListener("click", () => { 
