57489043ece7f31177792051f51b72060dc3dc1a
Adam Grant <hello@adamgrant.me>
2019-11-13 20:43:36 -0800

    ios improvements

--- js/events/utilities.js
+++ js/events/utilities.js
@@ -1,5 +1,6 @@
 m$.utilities.events(_$ => {
   document.body.addEventListener("keypress", e => {
-    if (e.keyCode == 32) m$.quizzer.api.start_quiz();
+    // if (e.keyCode == 32) m$.quizzer.api.start_quiz();
+    // This interferes with the answer entry.
   });
 });
